bb0459163061ad15310f2e00763f891cc7b9d49c
Sebok <ms77922@nsroot.net>
2018-05-02 17:15:44 +0200

    Update to startup script

--- start_jupyter.cmd
+++ start_jupyter.cmd
@@ -2,11 +2,14 @@ rem This script starts up Jupyter notebook in the right directory
 rem echo "Add conda to PATH"
 rem set PATH=<path_to_conda>;%PATH%
 echo "cd into directory"
-pushd c:\cqdhu\python_finance_course\
+pushd <package_install_location>
 echo "list envs"
 call conda info --envs
 echo "activate conda env"
-call activate C:\Users\MS77922\AppData\Local\Continuum\Miniconda2\envs\ceu_course
+call activate <ceu_course_name>
+echo "Update env"
+call conda install --file env\conda_env.txt
+call pip install -r env\requirements.txt
 echo "start Jupyter"
 jupyter notebook --NotebookApp.iopub_data_rate_limit=10000000000
 popd
